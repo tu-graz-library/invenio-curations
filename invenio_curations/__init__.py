626c260703a6a71727b5edfa6e6c0dd3aa0d059a
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-03-31 13:45:07 +0200

    release: v0.8.4

--- invenio_curations/__init__.py
+++ invenio_curations/__init__.py
@@ -11,7 +11,7 @@
 from .ext import InvenioCurations
 from .proxies import current_curations, current_curations_service
 
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 
 __all__ = (
     "InvenioCurations",
